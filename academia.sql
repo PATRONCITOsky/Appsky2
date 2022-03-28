a62c10f6e239a57eee3672b11d65327624e422c2
PATRONCITOsky <hhernadez144@misena.edu.co>
2022-03-28 14:10:00 -0500

    Se hace la validacion en docentes y se actualiza la BD

--- academia.sql
+++ academia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-03-2022 a las 23:26:28
+-- Tiempo de generación: 28-03-2022 a las 21:06:55
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.2
 
@@ -41,12 +41,40 @@ CREATE TABLE `cursos` (
 --
 
 INSERT INTO `cursos` (`id`, `nombre`, `description`, `imagen`, `created_at`, `updated_at`) VALUES
-(1, 'Astrología', 'Todo lo conocido del cosmos a tu disposición', 'public/irZmKU3nWlqSJ5L1ZbCGm3BAi97lZjqqhjRRUqX5.jpg', '2022-03-05 03:14:15', '2022-03-05 03:14:15'),
+(1, 'Astrología V', 'Todo lo conocido del cosmos a tu disposición', 'public/wROWY0iQa1c6zXz3MWcFeIa4s8YJndDMgdgpamFO.jpg', '2022-03-05 03:14:15', '2022-03-19 03:49:30'),
 (3, 'Física', 'A la semana te matas', 'public/H2RA4IbTFyCfDaHgHyHh7EREyqNgzMlUr2I5oS1F.png', '2022-03-05 03:16:34', '2022-03-05 03:16:34'),
 (4, 'Matemáticas', 'Se mató porque tenia muchos problemas', 'public/ADy7GCUafy0vcCZ2e2w0waK6qx5w8F8H9TodeZSq.jpg', '2022-03-05 03:17:53', '2022-03-05 03:17:53'),
 (5, 'Química', 'H2O', 'public/bcqrFY50OQtEHupksHG9BfrwUwtUVUy5bWYWkVhZ.jpg', '2022-03-05 03:23:35', '2022-03-05 03:23:35'),
 (6, 'Esquivar balas', 'Vivir en Latinoamerica', 'public/ghhNHmUF0Kw0LW0DiTyfkFh3ytKudKZwx6En6kmZ.jpg', '2022-03-11 23:56:18', '2022-03-11 23:56:18'),
-(7, 'Esquivar balas 2', 'Vivir en Latinoamérica pobre', 'public/Xl07DwQgF6VjBp4wxHtkxAfExgQLP5Kfkqtyq609.jpg', '2022-03-11 23:56:51', '2022-03-11 23:56:51');
+(7, 'Esquivar balas 2', 'Vivir en Latinoamérica pobre', 'public/Xl07DwQgF6VjBp4wxHtkxAfExgQLP5Kfkqtyq609.jpg', '2022-03-11 23:56:51', '2022-03-11 23:56:51'),
+(13, 'Joker', NULL, 'public/nuzhA0sXTzOtOJeYY8o5u5UOKUDFfukMCUCSLomC.jpg', '2022-03-25 20:13:45', '2022-03-25 20:13:45'),
+(14, 'Joker3', 'Había un chiste tan malo que le pegaba a los chistes pequeños', 'public/jn8insC18rUNH02fJEsmWSUAdnJ5nibBQO7T65J5.jpg', '2022-03-25 23:25:59', '2022-03-25 23:26:40');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `docentes`
+--
+
+CREATE TABLE `docentes` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nombre` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `apellidos` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `foto` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `titulo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cursoAsociado` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `docentes`
+--
+
+INSERT INTO `docentes` (`id`, `nombre`, `apellidos`, `foto`, `titulo`, `cursoAsociado`, `created_at`, `updated_at`) VALUES
+(2, 'Diego', 'Marin Cano', 'public/jntECmNXds8Qpk7uqkXQayum4xkCTvBB5CDnl4p2.jpg', 'Ingeniero', 'JAVA', '2022-03-25 23:55:17', '2022-03-28 18:36:46'),
+(3, 'Carlos Andres', 'Mora Agudelo', 'public/7xNWQg2EaO0oSWAyonLCA4iKvdOJgailpK53mv82.jpg', 'Ingeniero', 'PHP con Laravel', '2022-03-28 18:21:42', '2022-03-28 18:35:29'),
+(4, 'Jorge Luis', 'Raigoza Barahona', 'public/KIV1U07yD4LuOc6j1bIMzCLpIHzchLCYoFokwRcg.jpg', 'Ingeniero', 'Python', '2022-03-28 19:02:59', '2022-03-28 19:02:59');
 
 -- --------------------------------------------------------
 
@@ -86,7 +114,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (3, '2019_08_19_000000_create_failed_jobs_table', 1),
 (4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
 (5, '2022_02_18_224519_create_cursos_table', 1),
-(6, '2022_03_04_201231_add_imagen_to_cursos', 2);
+(6, '2022_03_04_201231_add_imagen_to_cursos', 2),
+(7, '2022_03_25_160155_create_docentes_table', 3);
 
 -- --------------------------------------------------------
 
@@ -146,6 +175,12 @@ ALTER TABLE `cursos`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `docentes`
+--
+ALTER TABLE `docentes`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
@@ -187,7 +222,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `cursos`
 --
 ALTER TABLE `cursos`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT de la tabla `docentes`
+--
+ALTER TABLE `docentes`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `failed_jobs`
@@ -199,7 +240,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT de la tabla `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `personal_access_tokens`
